9e1a46544c552b96c1e3683c018025c15b040693
Pablo Ezequiel Aguirre <62770364+PablitoDT14@users.noreply.github.com>
2021-03-27 11:03:16 -0300

    Add files via upload

---
+++ Clase 1/index.js
@@ -0,0 +1,49 @@
+// Parte 1
+// const mensaje = 'Hola desde el index js';
+//console.log('de lejos no se ve')
+// console.log(mensaje);
+
+// let gato ={
+//     "edad": 3,
+//     "nombre": 'Ruben',
+//     "maullar": function(){
+//         console.log('Miau')
+//     }
+// }
+
+// console.log(gato.nombre.toUpperCase());
+
+
+// Parte 2
+// let variable = 1;
+
+// console.log(variable);
+// let varString ='miVar';
+// let varNum=10;
+// let varBool=false;
+// let varUndef= undefined;
+// let varNull = null;
+// let varBigInt=BigInt(10);
+// let varSymbol= Symbol('symbol');
+
+// let fn = function(){
+//     console.log('holis')
+// }
+
+// Parte 3
+// let nombre = prompt('Ingrese su nombre');
+// let edad = prompt('Ingrese su edad');
+// let date = new Date();
+// let year = date.getFullYear();
+
+
+// console.log(typeof edad);
+// edad = parseInt(edad);
+
+// let dateUser = year - edad;
+
+// console.log(typeof edad);
+
+// alert ('Bienvenido '+nombre + ' nacido en el año '+dateUser);
+
+
---
+++ Clase 2/js/bucles.js
@@ -0,0 +1,9 @@
+console.log('Hola mundo desde ciclos');
+
+
+let frutas = ['manzana','banana','naranja','papaya'];
+
+// Ciclo For
+for (let i=0; i<frutas.length; i++){
+    console.log(frutas[i]);
+}
