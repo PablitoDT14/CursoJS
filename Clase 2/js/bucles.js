f437c67f0f35615429e0a87076cee9cc43824c43
PablitoDT14 <pablo.eze.aguirre@gmail.com>
2021-03-27 12:37:56 -0300

    actualizacion bucles clase 2

--- Clase 2/js/bucles.js
+++ Clase 2/js/bucles.js
@@ -1,9 +1,51 @@
 console.log('Hola mundo desde ciclos');
-
+/*
 
 let frutas = ['manzana','banana','naranja','papaya'];
 
 // Ciclo For
 for (let i=0; i<frutas.length; i++){
     console.log(frutas[i]);
+}
+ 
+for (let i=1; i<=10; i++){
+    if(i===5){
+        continue;
+    }
+    console.log(i);
+    
+}
+*/
+
+//WHILE
+/*
+let entrada = prompt("Ingresar un dato");
+while(entrada !== "ESC"){
+    alert("El usuario ingresó "+entrada);
+    entrada = prompt("Ingresar otro dato");
+}
+
+let frutas = ['manzana','banana','naranja','papaya'];
+let posicionFruta = 0;
+let cantidadElementosEnArray = frutas.length;
+
+while(posicionFruta<cantidadElementosEnArray){
+    console.log(frutas[posicionFruta]);
+    posicionFruta++;
+}
+*/
+//SWITCH
+
+let signo = prompt('De qué signo sos?');
+signo=signo.toLowerCase();
+switch(signo){
+    case 'tauro': 
+        console.log('horóscopo tauro');
+        break;
+    case 'cancer':
+        console.log('a vos no te lo leo');
+        break;
+    default:
+        console.log('Eso no es un signo que leamos');
+        break;
 }
