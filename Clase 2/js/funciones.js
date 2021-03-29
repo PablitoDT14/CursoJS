d1191e9aaa671d5451de3877838ce1c9a6bbc408
PablitoDT14 <pablo.eze.aguirre@gmail.com>
2021-03-29 09:11:31 -0300

    clase 2 (3y4)

---
+++ Clase 2/js/funciones.js
@@ -0,0 +1,50 @@
+function pedirNumeros(){
+
+}
+
+function pedirOperador(){
+
+}
+
+let num1= prompt('Ingrese un número');
+
+let operacion= prompt('Ingrese la operación que desea realizar + - * /');
+
+
+
+
+
+function sumar(num1, num2){
+    let suma=num1+num2;
+    console.log(suma);
+}
+
+sumar(1,3);
+sumar(5,6);
+
+
+/*
+function obtenerSigno(){
+    let signo = prompt('De qué signo sos?').toLowerCase();
+    return signo;
+}
+
+
+function leerHoroscopo(signo){
+    console.log(signo);
+        switch(signo){
+        case 'tauro': 
+            console.log('horóscopo tauro');
+            break;
+        case 'cancer':
+            console.log('a vos no te lo leo');
+            break;
+        default:
+            console.log('Eso no es un signo que leamos');
+            break;
+    }
+}
+
+let signos = obtenerSigno();
+leerHoroscopo(signos);
+*/
